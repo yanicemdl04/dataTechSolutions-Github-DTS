0b0160c48a4799b9f7671318b995fffd616ff892
Yanice <yanicemundele@gmail.com>
2023-10-29 09:23:11 +0100

    dts1

---
+++ JS & PHP/contact.js
@@ -0,0 +1,37 @@
+/** 
+ @param {string} selector 
+*/
+
+function animateTittle (selector) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const pic = document.querySelectorByClassName('animé')
+// let topPos = 0;
+// let dir = 1;
+
+// function hautBas (){
+//     if (topPos == 50){
+//         dir == -1
+//     } else if (topPos == -20){
+//         dir == 1
+//     }
+//     topPos += 2 * dir;
+//     animé.style.top = `${topPos}px`;
+//     requestAnimationFrame(hautBas)
+
+// }
+
+// requestAnimationFrame(hautBas)
